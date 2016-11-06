29fc7e7225535773b74d1040848cd44a0c9463a4
Hristiqn <hristiqnk@gmail.com>
2016-11-06 23:25:54 +0200

    A small site

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+       $(document).ready(function() {
+           $("#about").click(function() {
+               $('html, body').animate({
+                   scrollTop: $(".main").offset().top
+               }, 1000);
+           });
+
+           $("#works").click(function() {
+               $('html, body').animate({
+                   scrollTop: $(".works").offset().top
+               }, 1000);
+           });
+
+           $("#services").click(function() {
+               $('html, body').animate({
+                   scrollTop: $(".services").offset().top
+               }, 1000);
+           });
+
+           $("#contact").click(function() {
+               $('html, body').animate({
+                   scrollTop: $(".contact").offset().top
+               }, 1000);
+           });
+
+
+           $(window).scroll(function() {
+               var scroll = $(window).scrollTop();
+               //console.log(scroll);
+               if (scroll >= 50) {
+                   //console.log('a');
+                   $("header").addClass("change");
+               } else {
+                   //console.log('a');
+                   $("header").removeClass("change");
+               }
+           });
+
+           $('.toggleNav').on('click', function() {
+               $('nav').toggleClass('open');
+           });
+       });
